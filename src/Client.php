f54a2fc6274bb0cb7da15b09aa4d3c28d9caa8d9
Stanko <stankoar@gmail.com>
2021-05-05 09:56:42 +0200

    Batch requests

--- src/Client.php
+++ src/Client.php
@@ -5,10 +5,10 @@ namespace RoiChamp;
 class Client{
 
     const ENDPOINT = 'https://roichamp.com/api/v1/';
-    
+
     protected $ch;    
     protected $warnings;
-
+    protected $batchLimit;
     private $_token;
 
     public function __construct(array $config)
@@ -21,7 +21,17 @@ class Client{
         }
 
         $this->warnings = [];
+        $this->batchLimit = 20;
 
+    }
+
+    public function getBatchLimit(){
+        return $this->batchLimit;
+    }
+
+    public function setBatchLimit($limit){
+        $this->batchLimit = $limit;
+        return $this->batchLimit;
     }
 
     protected function endpoint($path){
@@ -59,6 +69,9 @@ class Client{
         return $this->post('product/upsert', $data);
     }
 
+    public function batchUpsertProduct(array $data){
+        return $this->post('product/batch-upsert', ['data' => $data]);
+    }
 
     public function upsertCategory(array $data){
 
@@ -73,6 +86,9 @@ class Client{
         return $this->post('category/upsert', $data);
     }
 
+    public function batchUpsertCategory(array $data){
+        return $this->post('category/batch-upsert', ['data' => $data]);
+    }
 
     public function upsertSubscriber(array $data){
 
@@ -85,6 +101,10 @@ class Client{
         }
 
         return $this->post('subscriber/upsert', $data);
+    }
+
+    public function batchUpsertSubscriber(array $data){
+        return $this->post('subscriber/batch-upsert', ['data' => $data]);
     }
 
     public function unsubscribe($email){
@@ -134,6 +154,10 @@ class Client{
         return $this->post('cart/upsert', $data);
     }
 
+    public function batchUpsertCart(array $data){
+        return $this->post('cart/batch-upsert', ['data' => $data]);
+    }
+
     public function upsertOrder(array $data){
 
         if( !isset( $data['identity'] ) ){
@@ -178,6 +202,10 @@ class Client{
         return $this->post('order/upsert', $data);
     }
 
+    public function batchUpsertOrder(array $data){
+        return $this->post('order/batch-upsert', ['data' => $data]);
+    }
+
     public function getCart($identity){
         return $this->get('cart/view', [
             'identity' => $identity
@@ -187,6 +215,10 @@ class Client{
    
     protected function initCurl(){
 
+        if( !$this->_token ){
+            return false;
+        }
+
         if( !$this->ch ){
             $this->ch = curl_init();
         }
@@ -194,11 +226,15 @@ class Client{
         $authorization = "Authorization: Bearer ".$this->_token;
         curl_setopt($this->ch, CURLOPT_HTTPHEADER, [$authorization]); 
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+
+        return true;
     }
 
     protected function post($path, $data){
 
-        $this->initCurl();
+        if(!$this->initCurl()){
+            return;
+        }
 
         curl_setopt($this->ch, CURLOPT_URL, $this->endpoint($path));
         curl_setopt($this->ch, CURLOPT_POST, 1);
@@ -220,7 +256,9 @@ class Client{
 
     protected function get($path, $data){
 
-        $this->initCurl();
+        if(!$this->initCurl()){
+            return;
+        }
 
         $path = $path . '?' . http_build_query($data);
 
